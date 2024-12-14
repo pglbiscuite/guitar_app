17bf6fc48ac26e68e174e4499ee242c7638b907c
pglbiscuite <114862250+pglbiscuite@users.noreply.github.com>
2024-12-14 19:42:46 +0200

    Added The Scales

---
+++ main.py
@@ -0,0 +1,11 @@
+from scales import *
+
+#   TODO build the main functions ideas
+#   TODO Linear Checking
+
+
+#   Check to see if the american_scale == the romanian_scale
+def scale_matching(scale_answer):
+    pass
+
+
---
+++ scales.py
@@ -0,0 +1,19 @@
+#   The Scales
+
+#   American Scale //
+american_scale = ['A',
+                  'B',
+                  'C',
+                  'D',
+                  'E',
+                  'F',
+                  'G']
+
+#   Romanian Scale //
+romanian_scale = ['La',
+                  'Sol',
+                  'Do',
+                  'Re',
+                  'Mi',
+                  'Fa',
+                  'Sol']
